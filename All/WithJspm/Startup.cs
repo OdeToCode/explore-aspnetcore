f7aa0a3d67c25c6a17f7849776b39ae17ed8bf53
K. Scott Allen <scott@OdeToCode.com>
2015-02-16 15:41:26 +0000

    starting point

---
+++ All/WithJspm/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.StaticFiles;
+
+namespace WithJspm
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseFileServer(new FileServerOptions
+            {
+                 EnableDefaultFiles = true,
+                 EnableDirectoryBrowsing = true,                   
+            });            
+        }
+    }
+}
