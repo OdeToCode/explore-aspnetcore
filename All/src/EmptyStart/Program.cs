c1ed697fe980a4ebf35bcbda5f42909b2330837b
K. Scott Allen <scott@OdeToCode.com>
2015-03-30 14:23:15 +0000

    more console experiements

--- All/src/EmptyStart/Program.cs
+++ All/src/EmptyStart/Program.cs
@@ -1,21 +1,33 @@
-﻿using Microsoft.Framework.DependencyInjection.ServiceLookup;
+﻿using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.Runtime;
 using System;
 
 namespace EmptyStart
 {
     public class Program
     {
-        public Program(IServiceManifest manifest)
+        public Program(ILibraryManager libraryManager)
         {
-            
+            foreach (var lib in libraryManager.GetLibraries())
+            {
+                Console.WriteLine(lib.Name);
+            }
         }
 
         public static void Main(string[] args)
         {
+            var config = new Configuration()
+                            .AddJsonFile("config.json")
+                            .AddCommandLine(args);
+
+            Console.WriteLine(config.Get("message"));
+
             foreach (var arg in args)
             {
                 Console.WriteLine(arg);
             }
+
+            Console.ReadLine();
         }
     }
 }
