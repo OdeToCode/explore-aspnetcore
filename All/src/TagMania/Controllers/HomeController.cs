d48865342c9770d6bdaa41ad09bc50c1e2915e4d
K. Scott Allen <scott@OdeToCode.com>
2015-05-07 21:17:38 -0400

    tag helper experiments

---
+++ All/src/TagMania/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace TagMania.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return Content("About");
+        }
+    }
+}
---
+++ All/src/TagMania/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.StaticFiles;
+using Microsoft.Framework.DependencyInjection;
+
+namespace TagMania
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {      
+            app.UseMvc(rb =>
+            {
+                rb.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+            });
+       
+        }
+    }
+}
