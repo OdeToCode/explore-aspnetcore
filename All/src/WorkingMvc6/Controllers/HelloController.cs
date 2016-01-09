0335f97118fc931f51af4ea3d049952e54a0ae73
K. Scott Allen <scott@OdeToCode.com>
2016-01-09 00:44:08 -0500

    mvc 6 session prep

---
+++ All/src/WorkingMvc6/Controllers/HelloController.cs
@@ -0,0 +1,35 @@
+using System.Runtime.Serialization;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace WorkingMvc6.Controllers
+{        
+    [Route("[controller]")]
+    public class HelloController : Controller
+    {
+        private IApplicationEnvironment _env;
+
+        public HelloController(IApplicationEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new ObjectResult(_env);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Services([FromServices] IServiceCollection services)
+        {            
+            return View(services);            
+        }
+
+    }
+  
+}
+
+
---
+++ All/src/WorkingMvc6/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json.Serialization;
+using WorkingMvc6.Controllers;
+
+namespace WorkingMvc6
+{
+    public class Startup
+    { 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvc(options =>
+                {
+                    options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+                })
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                })
+                .AddViewOptions(options =>
+                {
+                    options.HtmlHelperOptions.ClientValidationEnabled = false;
+                });
+                               
+            services.AddSingleton<IServiceCollection>(provider => services);
+        }
+        
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddDebug();
+            loggerFactory.MinimumLevel = LogLevel.Verbose;
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseMvc();
+
+            app.Run(async context =>
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("Route not found");
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
