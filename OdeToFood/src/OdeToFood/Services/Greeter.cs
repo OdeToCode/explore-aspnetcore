f4347fb4e35489f332ce5378b34a89eb6736a622
Scott <scott@OdeToCode.com>
2016-01-27 08:02:34 -0500

    m1 after

---
+++ OdeToFood/src/OdeToFood/Services/Greeter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["greeting"];
+        }
+
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
---
+++ OdeToFood/src/OdeToFood/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Services;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app,
+            IGreeter greeter)
+        {
+            app.UseIISPlatformHandler();
+
+            app.Run(async (context) =>
+            {
+                var greeting = greeter.GetGreeting();
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
