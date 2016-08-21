dd9faf74a2a1d8b487a92bac384e61af05f61d7f
Scott <scott@OdeToCode.com>
2016-08-20 22:47:59 -0400

    finished m4

--- OdeToFood/src/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/src/OdeToFood/Controllers/HomeController.cs
@@ -1,23 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OdeToFood.Models;
+using OdeToFood.Entities;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        private IGreeter _greeter;
         private IRestaurantData _restaurantData;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomePageViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetGreeting();
 
             return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = _restaurantData.Get(id);
+            if(model == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(RestaurantEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var newRestaurant = new Restaurant();
+                newRestaurant.Cuisine = model.Cuisine;
+                newRestaurant.Name = model.Name;
+                newRestaurant = _restaurantData.Add(newRestaurant);
+
+                return RedirectToAction("Details", new { id = newRestaurant.Id });
+            }
+            return View();
         }
     }
 }
