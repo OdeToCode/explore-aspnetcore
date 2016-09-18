0558fe0222f4b7f924db29bf38424feffb86d96a
K. Scott Allen <scott@OdeToCode.com>
2016-09-18 07:11:44 -0400

    show assembly versions and load path

--- All/src/Controllers/Program.cs
+++ All/src/Controllers/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Controllers
@@ -7,6 +9,15 @@ namespace Controllers
     {
         public static void Main(string[] args)
         {
+            var assemblies = Assembly.GetEntryAssembly().GetReferencedAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                Console.WriteLine($"{assembly.Version} {assembly.Name}");
+
+                var codebase = Assembly.Load(assembly).CodeBase.Replace("file:///", "");                               
+                Console.WriteLine($"\t{Path.GetDirectoryName(codebase)}");
+            }
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
