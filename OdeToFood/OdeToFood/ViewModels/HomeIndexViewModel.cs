47c3cd2e7a791bc1a7f5cdf68bb16b89953eee28
Scott Allen <scott@odetocode.com>
2017-09-27 13:39:15 -0400

    redo module 4 clip 2

---
+++ OdeToFood/OdeToFood/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OdeToFood.Models;
+using System.Collections.Generic;
+
+namespace OdeToFood.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public string CurrentMessage { get; set; }
+    }
+}
