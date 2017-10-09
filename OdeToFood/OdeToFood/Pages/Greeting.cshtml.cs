68c7bdd70ddecd4d13f280c2634da4f292c93f8a
Scott Allen <scott@odetocode.com>
2017-10-09 14:29:10 -0400

    m6 c5

---
+++ OdeToFood/OdeToFood/Pages/Greeting.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Services;
+
+namespace OdeToFood.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        private IGreeter _greeter;
+
+        public string CurrentGreeting { get; set; }
+
+        public GreetingModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public void OnGet(string name)
+        {
+            CurrentGreeting = $"{name}: {_greeter.GetMessageOfTheDay()}";
+        }
+    }
+}
