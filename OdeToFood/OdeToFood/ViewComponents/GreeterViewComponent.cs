41cb0aa7720c697a7de934677d817c0bca0967a9
Scott Allen <scott@odetocode.com>
2017-10-11 10:01:15 -0400

    complete m6

---
+++ OdeToFood/OdeToFood/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetMessageOfTheDay();
+            return View("Default", model);
+        }
+
+    }
+}
