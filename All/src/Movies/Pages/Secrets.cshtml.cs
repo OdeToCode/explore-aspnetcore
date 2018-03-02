e340ac2b157d984d1c7446bfeee3236e757c4c2e
Scott Allen <scott@odetocode.com>
2018-03-02 13:20:33 -0500

    adding secrets page

---
+++ All/src/Movies/Pages/Secrets.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Movies.Pages
+{
+    public class SecretsModel : PageModel
+    {
+        public void OnGet( )
+        {
+
+        }
+    }
+}
