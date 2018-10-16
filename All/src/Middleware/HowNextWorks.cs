20f1a31c922fddfd114ff7afdd06a253f1fe1086
Scott Allen <scott@odetocode.com>
2018-10-15 21:35:27 -0400

    how next works in middleware

---
+++ All/src/Middleware/HowNextWorks.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Builder.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Http.Features;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class TestHttpContext : HttpContext
+    {
+        public TestHttpContext()
+        {
+            Items = new Dictionary<object, object>();
+        }
+
+        public override IFeatureCollection Features => throw new NotImplementedException();
+
+        public override HttpRequest Request => throw new NotImplementedException();
+
+        public override HttpResponse Response => throw new NotImplementedException();
+
+        public override ConnectionInfo Connection => throw new NotImplementedException();
+
+        public override WebSocketManager WebSockets => throw new NotImplementedException();
+        
+
+        public override ClaimsPrincipal User { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override IDictionary<object, object> Items { get; set; }
+        public override IServiceProvider RequestServices { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override CancellationToken RequestAborted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string TraceIdentifier { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override ISession Session { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+#pragma warning disable CS0672 // Member overrides obsolete member
+        public override AuthenticationManager Authentication => throw new NotImplementedException();
+#pragma warning restore CS0672 // Member overrides obsolete member
+#pragma warning restore CS0618 // Type or member is obsolete
+
+        public override void Abort()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MiddlewareComponentNode
+    {
+        public RequestDelegate Next;
+        public RequestDelegate Process;
+        public Func<RequestDelegate, RequestDelegate> Component;
+    }
+
+    public interface ISimpleApplicationBuilder
+    {
+        void Use(Func<RequestDelegate, RequestDelegate> middleware);
+        RequestDelegate Build();        
+    }
+
+    public class SimpleApplicationBuilder : ISimpleApplicationBuilder
+    {
+        public RequestDelegate Build()
+        {
+            var node = Components.Last;
+            while(node != null)
+            {
+                node.Value.Next = GetNextFunc(node);
+                node.Value.Process = node.Value.Component(node.Value.Next);
+                node = node.Previous;
+            }
+            return Components.First.Value.Process;
+        }
+
+        private RequestDelegate GetNextFunc(LinkedListNode<MiddlewareComponentNode> node)
+        {
+            if(node.Next == null)
+            {
+                // no more middleware components left in the list 
+                return ctx =>
+                {
+                    // consider a 404 since no other middleware processed the request
+                    // ctx.Response.StatusCode = 404;
+                    return Task.CompletedTask;
+                };
+            }
+            else
+            {
+                return node.Next.Value.Process;
+            }
+        }
+
+        public void Use(Func<RequestDelegate, RequestDelegate> component)
+        {
+            var node = new MiddlewareComponentNode
+            {
+                Component = component
+            };
+
+            Components.AddLast(node);
+        }
+
+        LinkedList<MiddlewareComponentNode> Components = new LinkedList<MiddlewareComponentNode>();
+    }
+}
---
+++ All/test/Tests/Middleware/SimpleMiddlewareTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Middleware;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests.Middleware
+{
+    public static class HttpContextExtensions
+    {
+        public static void AddLogItem(this HttpContext context, string message)
+        {
+            var log = GetLogItem(context);
+            log.Add(message);
+        }
+
+        public static List<string> GetLogItem(this HttpContext context)
+        {
+            if (!context.Items.ContainsKey("log"))
+            {
+                context.Items.Add("log", new List<string>());
+            }
+
+            var log = context.Items["log"] as List<string>;           
+            return log;
+        }
+    }
+
+    public class SimpleMiddlewareTests
+    {
+        [Fact]
+        public void CanBuildPipeline()
+        {
+            var app = new SimpleApplicationBuilder();
+
+            app.Use(next =>
+            {
+                return async ctx =>
+                {
+                    ctx.AddLogItem("Enter middleware 1");
+                    await next(ctx);
+                    ctx.AddLogItem("Exit middleware 1");
+                };
+            });
+
+            app.Use(next =>
+            {
+                return async ctx =>
+                {
+                    ctx.AddLogItem("Enter middleware 2");
+                    await next(ctx);
+                    ctx.AddLogItem("Exit middleware 2");
+                };
+            });
+
+            app.Use(next =>
+            {
+                return async ctx =>
+                {
+                    ctx.AddLogItem("Enter middleware 3");
+                    await next(ctx);
+                    ctx.AddLogItem("Exit middleware 3");
+                };
+            });
+
+            var pipeline = app.Build();
+
+            var request = new TestHttpContext();
+            pipeline(request);
+
+            var log = request.GetLogItem();
+
+            Assert.Equal(6, log.Count);
+            Assert.Equal("Enter middleware 1", log[0]);
+            Assert.Equal("Exit middleware 1", log[5]);
+        }
+    }
+}
