fc7cf96ce22cdf95cc019db36426236e89c166b2
Scott Allen <scott@odetocode.com>
2019-09-27 09:09:34 -0400

    figuring out how DbContextOptions are added to service collection

--- All/test/TestTheTest/TestInMemory/CustomWebFactory.cs
+++ All/test/TestTheTest/TestInMemory/CustomWebFactory.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
 
 namespace TestInMemory
 {
@@ -21,11 +24,37 @@ namespace TestInMemory
         {
             builder.ConfigureServices(services =>
             {
+                //
+                // This won't replace the DbContextOptions that are already in place 
+                //
                 services.AddDbContext<SomeDbContext>(options =>
                 {
                     options.UseInMemoryDatabase("DB");
                 });
+
+                //
+                // This will replace DbContextOptions
+                //
+                //AddCoreServices<SomeDbContext>(services, (p, o) =>
+                //{
+                //    o.UseInMemoryDatabase("DB");
+                //}, ServiceLifetime.Scoped);
+
             });
+        }
+
+        private static void AddCoreServices<TContextImplementation>(IServiceCollection serviceCollection, Action<IServiceProvider, DbContextOptionsBuilder> optionsAction, ServiceLifetime optionsLifetime) where TContextImplementation : DbContext
+        {
+            serviceCollection.Add(new ServiceDescriptor(typeof(DbContextOptions<TContextImplementation>), (IServiceProvider p) => DbContextOptionsFactory<TContextImplementation>(p, optionsAction), optionsLifetime));
+            serviceCollection.Add(new ServiceDescriptor(typeof(DbContextOptions), (IServiceProvider p) => p.GetRequiredService<DbContextOptions<TContextImplementation>>(), optionsLifetime));
+        }
+
+        private static DbContextOptions<TContext> DbContextOptionsFactory<TContext>(IServiceProvider applicationServiceProvider, Action<IServiceProvider, DbContextOptionsBuilder> optionsAction) where TContext : DbContext
+        {
+            DbContextOptionsBuilder<TContext> dbContextOptionsBuilder = new DbContextOptionsBuilder<TContext>(new DbContextOptions<TContext>(new Dictionary<Type, IDbContextOptionsExtension>()));
+            dbContextOptionsBuilder.UseApplicationServiceProvider(applicationServiceProvider);
+            optionsAction?.Invoke(applicationServiceProvider, dbContextOptionsBuilder);
+            return dbContextOptionsBuilder.Options;
         }
     }
 }
